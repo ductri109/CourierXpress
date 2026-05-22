7aff861098292c47b608b8392e071c0356b81f8e
Bui Duc Tri <buiductri179@gmail.com>
2026-05-22 19:10:19 +0700

    Ép forceScheme HTTPS để sửa lỗi bảo mật Form Đăng nhập

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Providers;
-
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +19,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Ép Laravel luôn sinh link HTTPS khi chạy trên máy chủ Render
+        if (config('app.env') === 'production' || env('DATABASE_URL')) {
+            URL::forceScheme('https');
+        }
     }
 }
